a699dfdaebc5f6b89907a2b88b4b8e3113770b10
username34 <chris.rogers@mail.helenacollege.edu>
2022-11-03 11:58:19 -0600

    added users/admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
+# accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'is_staff', ]
+
+
+fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('username',)}),)
+add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('username',)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
