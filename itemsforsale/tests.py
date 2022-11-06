757030bb5dded46fe93902c8bf13b7e666c88f4b
username34 <chris.rogers@mail.helenacollege.edu>
2022-11-06 16:51:54 -0700

    most of ch 10

--- itemsforsale/tests.py
+++ itemsforsale/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
+
+class HomePageTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
 # Create your tests here.
