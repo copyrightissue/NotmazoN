e78655910681841cce495f445826f01d79d39e9e
username34 <chris.rogers@mail.helenacollege.edu>
2022-11-06 16:51:54 -0700

    most of ch 10

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,74 @@
+# accounts/tests.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='jacob', email='jacob@…', password='top_secret'
+        )
+        self.assertEqual(user.username, 'jacob')
+        self.assertEqual(user.email, 'jacob@…')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superuser', email='superuser@…', password='top_secret'
+        )
+        self.assertEqual(admin_user.username, 'superuser')
+        self.assertEqual(admin_user.email, 'superuser@…')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+    def test_signup_page(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+        self.assertContains(response, 'Sign Up')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@email.com",
+                "password1": "testpass123",
+                "password2": "testpass123",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser@email.com")
+
+    def test_login_page(self):
+        url = reverse('login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertContains(response, 'Log In')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
+
+    # def test_logout_page(self):
+    #     url = reverse('logout')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'registration/logged_out.html')
+    #     self.assertContains(response, 'Logged Out')
+    #     self.assertNotContains(
+    #         response, 'Hi there! I should not be on the page.')
 
 # Create your tests here.
