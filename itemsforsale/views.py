87418d6f7c9e8a98329f42a0f449e73a197605a3
KaelanC <107579663+theArzt-KaelanC@users.noreply.github.com>
2022-11-17 12:11:32 -0700

    permissions

--- itemsforsale/views.py
+++ itemsforsale/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -9,12 +10,12 @@ class ItemListView(ListView):
     template_name = 'item_list.html'
 
 
-class ItemDetailView(DetailView):
+class ItemDetailView(LoginRequiredMixin, DetailView):
     model = Item
     template_name = "item_detail.html"
 
 
-class ItemUpdateView(UpdateView):
+class ItemUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Item
     fields = (
         "title",
@@ -23,19 +24,30 @@ class ItemUpdateView(UpdateView):
         )
     template_name = "item_edit.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ItemDeleteView(DeleteView):
+
+class ItemDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Item
     template_name = "item_delete.html"
     success_url = reverse_lazy("item_list")
 
+    def test_func(self): 
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ItemCreateView(CreateView):
+
+class ItemCreateView(LoginRequiredMixin, CreateView):
     model = Item
     template_name = "item_new.html"
     fields = (
         "title",
         "image",
         "price",
-        "author",
     )
+
+    def form_valid(self, form): 
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
