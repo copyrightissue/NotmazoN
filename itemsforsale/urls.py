f5930482f0eeee6dced0a9b00492efcfcef4fe98
KaelanC <107579663+theArzt-KaelanC@users.noreply.github.com>
2022-12-01 12:11:42 -0700

    search bar

--- itemsforsale/urls.py
+++ itemsforsale/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ItemListView, ItemDetailView, ItemDeleteView, ItemCreateView, ItemUpdateView
+from .views import ItemListView, ItemDetailView, ItemDeleteView, ItemCreateView, ItemUpdateView, SearchResultsView
 
 urlpatterns = [
     path("<int:pk>/", ItemDetailView.as_view(), name="item_detail"), 
     path("<int:pk>/edit/", ItemUpdateView.as_view(), name="item_edit"),
     path("<int:pk>/delete/", ItemDeleteView.as_view(), name="item_delete"),
     path("new/", ItemCreateView.as_view(), name="item_new"),
+    path("search/", SearchResultsView.as_view(), name="search_results"),
     path('', ItemListView.as_view(), name="item_list"),
     ]
