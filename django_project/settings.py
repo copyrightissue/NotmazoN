cc1e7052b19bc31ed5f649a025f50003a5bac501
username34 <chris.rogers@mail.helenacollege.edu>
2022-12-11 21:31:23 -0700

    fixed email

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,7 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "")
 MEDIA_URL = "/"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-DEFAULT_FROM_EMAIL = "kc195609@umconnect.umt.edu" 
+DEFAULT_FROM_EMAIL = "RogersChristopher2@gmail.com"
 EMAIL_HOST = "smtp.sendgrid.net" 
 EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
